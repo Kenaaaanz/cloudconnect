682597ce920c4bf8be360dfcbf2605cbb06351b4
kenaaaanz <gichabakenani@gmail.com>
2025-10-30 19:29:47 +0300

    better requirements handling

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # Install dependencies
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --noinput
--- cloudconnect/settings.py
+++ cloudconnect/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     '.onrender.com',
-    '.up.railway.app',
+    'https://cloudconnect-sjm0.onrender.com',
 ]
 
 
